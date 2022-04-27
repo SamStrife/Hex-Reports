50623cd2306d3b0456366810cc3d6e6d08c84967
Sam Fletcher <sam.a.fletcher@gmail.com>
2022-04-27 16:10:37 +0100

    Removed component aspect and put in layout file to simplify process

--- src/pages/GitiCrouchSplit.vue
+++ src/pages/GitiCrouchSplit.vue
@@ -1,7 +1,17 @@
 <template>
   <q-page>
     <div class="q-pa-md">
-      <GitiCrouchUploader></GitiCrouchUploader>
+      <q-file
+        v-model="file"
+        label="Upload File"
+        outlined
+        use-chips
+        accept=".xlsx"
+      />
+      <br />
+      <q-btn color="primary" label="Process" />
+      <br />
+      <p>{{ apiReturn }}</p>
     </div>
 
     <div class="q-pa-md">
@@ -53,15 +63,22 @@
 
 <script>
 import { ref } from "vue";
-import GitiCrouchUploader from "../components/GitiCrouchUploader.vue";
+import axios from "axios";
 
 export default {
-  components: {
-    GitiCrouchUploader,
-    GitiCrouchUploader,
-  },
   setup() {
-    return {};
+    const file = ref("");
+    const apiReturn = ref("");
+
+    axios.get("http://127.0.0.1:5000/").then((response) => {
+      console.log(response);
+      apiReturn.value = response.data;
+    });
+
+    return {
+      file,
+      apiReturn,
+    };
   },
 };
 </script>
