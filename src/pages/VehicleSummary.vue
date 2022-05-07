ac7d2b9eef9b4d8cc85c60e6da23ea8568703062
Sam Fletcher <sam.a.fletcher@gmail.com>
2022-05-07 14:55:55 +0100

    Added location to Vehicle Summary Report and finished display validation

--- src/pages/VehicleSummary.vue
+++ src/pages/VehicleSummary.vue
@@ -12,7 +12,10 @@
         />
         <q-btn type="submit" label="Get Details" color="green-10" />
       </q-form>
-      <div>
+      <div v-if="!queryRanOnce">
+        <div class="q-pa-md"></div>
+      </div>
+      <div v-else-if="queryRanOnce && requestReturn.manufacturer">
         <div class="q-pa-md">
           <q-card>
             <q-card-section class="bg-primary text-white">
@@ -47,6 +50,19 @@
             <q-separator />
             <div class="q-pa-md text-h6" v-if="requestReturn?.vehicleType">
               {{ requestReturn.vehicleType }}
+            </div>
+            <div class="q-pa-md text-h6" v-else><br /></div>
+          </q-card>
+          <br />
+        </div>
+        <div class="q-pa-md">
+          <q-card>
+            <q-card-section class="bg-primary text-white">
+              <div class="text-h6">Location</div>
+            </q-card-section>
+            <q-separator />
+            <div class="q-pa-md text-h6" v-if="requestReturn?.location">
+              {{ requestReturn.location }}
             </div>
             <div class="q-pa-md text-h6" v-else><br /></div>
           </q-card>
@@ -162,7 +178,7 @@
           <br />
         </div>
       </div>
-      <div>
+      <div v-else>
         <div class="q-pa-md">
           <q-card>
             <q-card-section class="bg-red-10 text-white">
@@ -194,6 +210,7 @@ export default {
       manufacturer: null,
       model: null,
       vehicleType: null,
+      location: null,
       status: null,
       subStatus: null,
       customer: null,
@@ -206,7 +223,7 @@ export default {
       monthsUntilTacho: null,
     });
 
-    const hideResults = ref(true);
+    const queryRanOnce = ref(false);
 
     const vehicleNotFound = computed(() => {
       if (requestReturn.value.manufacturer) {
@@ -219,12 +236,13 @@ export default {
     function requestData() {
       axios
         .get(
-          `http://127.0.0.1:5000/VehicleSummary/${vehicleRegistration.value}`
+          `http://139.59.171.54:5000/VehicleSummary/${vehicleRegistration.value}`
         )
         .then((response) => {
           requestReturn.value.manufacturer = response?.data?.manufacturer[0];
           requestReturn.value.model = response?.data?.model[0];
           requestReturn.value.vehicleType = response?.data?.vehicleType[0];
+          requestReturn.value.location = response?.data?.location[0];
           requestReturn.value.status = response?.data?.status[0];
           requestReturn.value.subStatus = response?.data?.subStatus[0];
           requestReturn.value.customer = response?.data?.customer[0];
@@ -256,7 +274,8 @@ export default {
             new Date(),
             "months"
           );
-          hideResults.value = false;
+          console.log(response);
+          queryRanOnce.value = true;
         });
     }
 
@@ -288,7 +307,7 @@ export default {
       setMOTClass,
       setTachoClass,
       vehicleNotFound,
-      hideResults,
+      queryRanOnce,
     };
   },
 };
