b5fee6dd937771de32087727a9d131659b3d3a2d
Sam Fletcher <sam.a.fletcher@gmail.com>
2022-10-03 10:00:44 +0100

    Changed routes to be dynamically created based on menuList items

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,106 +1,134 @@
-const routes = [
-  {
-    path: "/",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
-  },
-  {
-    path: "/excesscalculator",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      {
-        path: "",
-        component: () => import("pages/ExcessMileageCalculator.vue"),
-      },
-    ],
-  },
-  {
-    path: "/thenextthing",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      {
-        path: "",
-        component: () => import("pages/ComingSoon.vue"),
-      },
-    ],
-  },
-  {
-    path: "/deliverycalculator",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      {
-        path: "",
-        component: () => import("pages/DeliveryCalculator.vue"),
-      },
-    ],
-  },
-  {
-    path: "/vehiclesummary",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      {
-        path: "",
-        component: () => import("pages/VehicleSummary.vue"),
-      },
-    ],
-  },
-  {
-    path: "/derbyyardsheet",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      {
-        path: "",
-        component: () => import("pages/DerbyYardSheet.vue"),
-      },
-    ],
-  },
-  {
-    path: "/assetfile",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      {
-        path: "",
-        component: () => import("pages/AssetFile.vue"),
-      },
-    ],
-  },
-  {
-    path: "/taxchecker",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      {
-        path: "",
-        component: () => import("pages/TaxChecker.vue"),
-      },
-    ],
-  },
-  {
-    path: "/DailySpendKPIDemo",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      {
-        path: "",
-        component: () => import("pages/DailySpendKPIDemo.vue"),
-      },
-    ],
-  },
-  {
-    path: "/showingstuff",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      {
-        path: "",
-        component: () => import("pages/ShowingStuff.vue"),
-      },
-    ],
-  },
+import { menuList } from "src/layouts/menuList";
 
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: "/:catchAll(.*)*",
-    component: () => import("pages/ErrorNotFound.vue"),
-  },
-];
+const routes = [];
+
+routes.push({
+  path: "/",
+  component: () => import("layouts/MainLayout.vue"),
+  children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+});
+
+menuList.forEach((item) =>
+  routes.push({
+    path: item.routerLink,
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import(`pages/${item.vueFile}`),
+      },
+    ],
+  })
+);
+
+routes.push({
+  path: "/:catchAll(.*)*",
+  component: () => import("pages/ErrorNotFound.vue"),
+});
+
+// const routes = [
+//   {
+//     path: "/",
+//     component: () => import("layouts/MainLayout.vue"),
+//     children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+//   },
+//   {
+//     path: "/excesscalculator",
+//     component: () => import("layouts/MainLayout.vue"),
+//     children: [
+//       {
+//         path: "",
+//         component: () => import("pages/ExcessMileageCalculator.vue"),
+//       },
+//     ],
+//   },
+//   {
+//     path: "/thenextthing",
+//     component: () => import("layouts/MainLayout.vue"),
+//     children: [
+//       {
+//         path: "",
+//         component: () => import("pages/ComingSoon.vue"),
+//       },
+//     ],
+//   },
+//   {
+//     path: "/deliverycalculator",
+//     component: () => import("layouts/MainLayout.vue"),
+//     children: [
+//       {
+//         path: "",
+//         component: () => import("pages/DeliveryCalculator.vue"),
+//       },
+//     ],
+//   },
+//   {
+//     path: "/vehiclesummary",
+//     component: () => import("layouts/MainLayout.vue"),
+//     children: [
+//       {
+//         path: "",
+//         component: () => import("pages/VehicleSummary.vue"),
+//       },
+//     ],
+//   },
+//   {
+//     path: "/derbyyardsheet",
+//     component: () => import("layouts/MainLayout.vue"),
+//     children: [
+//       {
+//         path: "",
+//         component: () => import("pages/DerbyYardSheet.vue"),
+//       },
+//     ],
+//   },
+//   {
+//     path: "/assetfile",
+//     component: () => import("layouts/MainLayout.vue"),
+//     children: [
+//       {
+//         path: "",
+//         component: () => import("pages/AssetFile.vue"),
+//       },
+//     ],
+//   },
+//   {
+//     path: "/taxchecker",
+//     component: () => import("layouts/MainLayout.vue"),
+//     children: [
+//       {
+//         path: "",
+//         component: () => import("pages/TaxChecker.vue"),
+//       },
+//     ],
+//   },
+//   {
+//     path: "/DailySpendKPIDemo",
+//     component: () => import("layouts/MainLayout.vue"),
+//     children: [
+//       {
+//         path: "",
+//         component: () => import("pages/DailySpendKPIDemo.vue"),
+//       },
+//     ],
+//   },
+//   {
+//     path: "/showingstuff",
+//     component: () => import("layouts/MainLayout.vue"),
+//     children: [
+//       {
+//         path: "",
+//         component: () => import("pages/ShowingStuff.vue"),
+//       },
+//     ],
+//   },
+
+//   // Always leave this as last one,
+//   // but you can also remove it
+//   {
+//     path: "/:catchAll(.*)*",
+//     component: () => import("pages/ErrorNotFound.vue"),
+//   },
+// ];
 
 export default routes;
