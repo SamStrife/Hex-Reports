76b12f9427d17ca06ac266fea698a5f908cb42ed
Sam Fletcher <sam.a.fletcher@gmail.com>
2023-03-17 15:05:06 +0000

    Created layout for Rental Audit Page

--- src/pages/RentalAgreementAudit.vue
+++ src/pages/RentalAgreementAudit.vue
@@ -1,13 +1,16 @@
 <template>
-  <q-page class="column flex-center">
-    <div class="section">
-      <h1>Stats</h1>
-    </div>
-    <div class="section">
-      <h1>Rental Table</h1>
-    </div>
-    <div class="section">
-      <h1>Document Tables</h1>
+  <q-page class="flex-center">
+    <div class="column">
+      <div class="section">
+        <h1>Stats</h1>
+      </div>
+      <div class="section">
+        <h1>Rental Table</h1>
+      </div>
+      <div class="row section">
+        <div class="subsection"><h1>Agreements</h1></div>
+        <div class="subsection"><h1>Check Sheets</h1></div>
+      </div>
     </div>
   </q-page>
 </template>
@@ -19,5 +22,12 @@
   border: solid;
   border-color: black;
   width: 80vw;
+  display: flex;
+  justify-content: space-between;
+}
+
+.subsection {
+  border: solid;
+  border-color: black;
 }
 </style>
