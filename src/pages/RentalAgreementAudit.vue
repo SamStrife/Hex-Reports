198e7624a067ac2a9ea48008b96540bc54ec88aa
SamStrife <87466272+SamStrife@users.noreply.github.com>
2023-03-21 09:23:58 +0000

    Working on table layouts for Rental Aduit Report

--- src/pages/RentalAgreementAudit.vue
+++ src/pages/RentalAgreementAudit.vue
@@ -2,45 +2,163 @@
   <q-page class="flex-center">
     <div class="column">
       <div class="section">
-        <h1>{{ totalHires }}</h1>
+        <h1>{{ audits.totalRentals }}</h1>
       </div>
-      <div class="section">
-        <h1>Rental Table</h1>
+      <div>
+        <div class="q-pa-md">
+          <q-table
+            class="my-sticky-dynamic"
+            title="Rentals"
+            :rows="audits.rentalData"
+            :columns="rentalColumns"
+            :loading="loading"
+            row-key="index"
+            virtual-scroll
+            :virtual-scroll-item-size="48"
+            :virtual-scroll-sticky-size-start="48"
+            :pagination="pagination"
+            :rows-per-page-options="[0]"
+            @virtual-scroll="onScroll"
+          />
+        </div>
       </div>
-      <div class="row section">
-        <div class="subsection"><h1>Agreements</h1></div>
-        <div class="subsection"><h1>Check Sheets</h1></div>
+      <div class="row">
+        <div>
+          <div class="q-pa-md">
+            <q-table
+              class="my-sticky-dynamic"
+              title="Rental Agreements For Hire"
+              :rows="audits.rentalData"
+              :columns="rentalColumns"
+              :loading="loading"
+              row-key="index"
+              virtual-scroll
+              :virtual-scroll-item-size="48"
+              :virtual-scroll-sticky-size-start="48"
+              :pagination="pagination"
+              :rows-per-page-options="[0]"
+              @virtual-scroll="onScroll"
+            />
+          </div>
+        </div>
+        <div>
+          <div class="q-pa-md">
+            <q-table
+              class="my-sticky-dynamic"
+              title="Check Outs For Hire"
+              :rows="audits.rentalData"
+              :columns="rentalColumns"
+              :loading="loading"
+              row-key="Unique ID"
+              virtual-scroll
+              :virtual-scroll-item-size="48"
+              :virtual-scroll-sticky-size-start="48"
+              :pagination="pagination"
+              :rows-per-page-options="[0]"
+              @virtual-scroll="onScroll"
+            >
+              <template v-slot:body="row">
+                <q-tr :props="row" @click="onRowClick(rowClicked())">
+                  <q-td key="agreementNumber">
+                    <p>{{ row["Unique ID"] }}</p>
+                  </q-td>
+                </q-tr>
+              </template>
+            </q-table>
+          </div>
+        </div>
       </div>
     </div>
   </q-page>
 </template>
 
 <script setup>
-import { ref, onBeforeMount, onUpdated } from "vue";
+import { ref, onBeforeMount } from "vue";
 import { useRentalAuditStore } from "src/stores/RentalAuditStore";
 
 const audits = useRentalAuditStore();
-
-const totalHires = ref();
+const loading = ref(false);
 
 onBeforeMount(() => {
   audits.getData();
 });
 
-totalHires.value = audits.totalRentals;
+const rentalColumns = [
+  {
+    name: "agreementNumber",
+    label: "Agrrement Number",
+    align: "left",
+    field: (row) => row["Unique ID"],
+  },
+  {
+    name: "registration",
+    required: true,
+    label: "Registration",
+    align: "left",
+    field: (row) => row["Registration"],
+    sortable: true,
+  },
+  {
+    name: "customer",
+    label: "Customer",
+    align: "left",
+    field: (row) => row["Customer Name"],
+    sortable: true,
+  },
+  {
+    name: "hireStart",
+    label: "Hire Start Date",
+    align: "left",
+    field: (row) => row["Hire Start Date"],
+  },
+  {
+    name: "hireType",
+    label: "Hire Type",
+    align: "left",
+    field: (row) => row["Hire Type Name"],
+  },
+  {
+    name: "live",
+    label: "Live Hire",
+    align: "left",
+    field: (row) => (row["Live"] ? "Yes" : null),
+  },
+  {
+    name: "rentalAgreement",
+    label: "Rental Agreement ?",
+    align: "left",
+    field: (row) => (row["RADocNumber"] ? "Yes" : null),
+  },
+  {
+    name: "checkOut",
+    label: "Check Out ?",
+    align: "left",
+    field: (row) => (row["CheckOutDocNumber"] ? "Yes" : null),
+  },
+];
+
+function rowClicked() {
+  console.log("Clicked");
+}
 </script>
 
-<style>
-.section {
-  border: solid;
-  border-color: black;
-  width: 80vw;
-  display: flex;
-  justify-content: space-between;
-}
+<style scoped lang="sass">
+.my-sticky-dynamic
+  /* height or max-height is important */
+  height: 410px
 
-.subsection {
-  border: solid;
-  border-color: black;
-}
+  .q-table__top,
+  .q-table__bottom,
+  thead tr:first-child th /* bg color is important for th; just specify one */
+    background-color: #fff
+
+  thead tr th
+    position: sticky
+    z-index: 1
+  /* this will be the loading indicator */
+  thead tr:last-child th
+    /* height of all previous header rows */
+    top: 48px
+  thead tr:first-child th
+    top: 0
 </style>
--- src/stores/RentalAuditStore.js
+++ src/stores/RentalAuditStore.js
@@ -11,17 +11,36 @@ export const useRentalAuditStore = defineStore("RentalAudit", {
     totalRentals() {
       return this.rentalData.length;
     },
+    rentalAgreements() {
+      return this.documentData.filter(
+        (document) => document["type"] == "rental"
+      );
+    },
+    checkOuts() {},
     totalRentalsChecked() {},
     totalDocumentsPresent(documentType) {},
   },
 
   actions: {
     async getData() {
+      this.rentalData = [];
       await axios
         .get("https://api.hexreports.com/rentalAudit/getAuditData")
         .then((response) => {
           for (let agreement of response.data.RAAudit) {
             this.rentalData.push(agreement);
+          }
+        });
+    },
+    async getDocumentsForRental(vehicleID) {
+      this.documentData = [];
+      await axios
+        .get(
+          `https://api.hexreports.com/rentalAudit/getDocumentData/${vehicleID}`
+        )
+        .then((response) => {
+          for (let document of response.data.Documents) {
+            this.documentData.push(document);
           }
         });
     },
